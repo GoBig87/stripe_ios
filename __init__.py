626bf4bcb4c15ad5a6ea5a4e9ea946817f513bb7
GoBig87 <nobody5390@yahoo.com>
2019-01-31 20:31:00 -0800

    removing multi arch lipo

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from toolchain import CythonRecipe, shprint
 from os.path import join
 from distutils.dir_util import copy_tree
+from shutil import copyfile
 import fnmatch
 import sh
 import os
@@ -8,12 +9,12 @@ import os
 class StripeRecipe(CythonRecipe):
     version = "1.0.0"
     url = "https://github.com/GoBig87/Stripe_iOS_Wrapper/archive/{version}.zip"
-    library = "stripe.a"
+    library = "libstripe.a"
     pbx_frameworks = ['Foundation','Stripe']
     depends = ["python","hostpython"]
     pre_build_ext = True
     archs = ['arm64']
-
+    
     def install(self):
         pass
         arch = list(self.filtered_archs)[0]
@@ -23,13 +24,9 @@ class StripeRecipe(CythonRecipe):
             pass
         dist_dir  = join(self.ctx.dist_dir,'root','python','lib','python2.7','site-packages','stripe')
         copy_tree(build_dir, dist_dir)
-
-    def biglink(self):
-        dirs = []
-        for root, dirnames, filenames in os.walk(self.build_dir):
-            if fnmatch.filter(filenames, "*.so.*"):
-                dirs.append(root)
-        cmd = sh.Command(join(self.ctx.root_dir, "tools", "biglink"))
-        shprint(cmd, join(self.build_dir, "stripe.a"), *dirs)
+    
+    def make_lipo(self, filename, library=None):
+        #Skip this since it's only one Arch
+        pass
 
 recipe = StripeRecipe()
